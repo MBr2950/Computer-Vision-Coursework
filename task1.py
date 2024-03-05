47b17cc69f25ddbb47d64a46e106cad514f3219a
Eza <tomlewisint@gmail.com>
2024-03-05 12:59:24 +0000

    Add custom edges to max's original working version

--- task1.py
+++ task1.py
@@ -40,12 +40,92 @@ def findDistance(points):
 
     return distance
 
+
 # Use canny edge detection to identify edge in image
 # Input: Image in the form of a numpy array
 # Output: Edgemap in the form of a numpy array
-def findEdges(image):
-    edges = cv2.Canny(image, 100, 200)
+def findEdges(image, highFraction = 0.6, lowFraction = 0.2):
+    #perform canny edge detection without use of cv2.Canny
+
+
+    #find gradient
+    sobelX = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
+    sobelY = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+
+    #find magnitude of gradient
+    intensity = np.sqrt(sobelX**2 + sobelY**2)
+    intensity = intensity / np.max(intensity) * 255
+    edges = intensity.astype(np.uint8)
+
+    #find direction of gradient
+    direction = np.arctan2(sobelY, sobelX)
+
+    #NON-MAX SUPPRESSION
+    supressionMat = np.zeros_like(edges)
+
+
+
+    for y in range(1, edges.shape[0] - 1):
+        for x in range(1, edges.shape[1] - 1):
+            
+            angle = direction[y, x]
+            pi = np.pi
+            
+            f = 255
+            b = 255
+            
+            #right
+            if (0 <= angle < pi / 8) or (7 * pi / 8 <= angle <= pi):
+                f = edges[y, x + 1]
+                b = edges[y, x - 1]
+            #down-right
+            elif (pi / 8 <= angle < 3 * pi / 8):
+                f = edges[y + 1, x - 1]
+                b = edges[y - 1, x + 1]
+            #down
+            elif (3 * pi / 8 <= angle < 5 * pi / 8):
+                f = edges[y + 1, x]
+                b = edges[y - 1, x]
+            #down-left
+            elif (5 * pi / 8 <= angle < 7 * pi / 8):
+                f = edges[y - 1, x - 1]
+                b = edges[y + 1, x + 1]
+
+            if (edges[y, x] >= f) and (edges[y, x] >= b):
+                supressionMat[y, x] = edges[y, x]
+
+    edges = supressionMat
+
+    #DOUBLE THRESHOLD
+    highThreshold = edges.max() * highFraction
+    lowThreshold = edges.max() * lowFraction
+
+    thresholdMat = np.zeros_like(edges)
+
+    strongI = np.where(edges > highThreshold)
+    weakI = np.where((edges <= highThreshold) & (edges >= lowThreshold))
+
+    thresholdMat[strongI] = 255
+    thresholdMat[weakI] = 50
+
+    edges = thresholdMat
+
+    
+    #HYSTERESIS
+
+    for y in range(1, edges.shape[0] - 1):
+        for x in range(1, edges.shape[1] - 1):
+            if (edges[y, x] == 50):
+                if (edges[y - 1, x - 1] == 255) or (edges[y - 1, x] == 255) or \
+                (edges[y - 1, x + 1] == 255) or (edges[y, x - 1] == 255) or \
+                (edges[y, x + 1] == 255) or (edges[y + 1, x - 1] == 255) or \
+                (edges[y + 1, x] == 255) or (edges[y + 1, x + 1] == 255):
+                    edges[y, x] = 255
+                else:
+                    edges[y, x] = 0
+
     return edges
+
 
 # Convert edgemap into lines
 # Input: Edgemap in the form of a numpy array
